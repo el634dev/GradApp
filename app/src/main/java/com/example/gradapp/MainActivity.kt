e6e939a41bc6388a9dbc66e96cb081481f1efa3e
el634dev <al7758066@gmail.com>
2026-01-20 12:08:51 -0500

    Finished styling and adding an image

--- app/src/main/java/com/example/gradapp/MainActivity.kt
+++ app/src/main/java/com/example/gradapp/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,15 +41,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Graduation Announcement",
-        textAlign = TextAlign.Center,
-        fontSize = 50.sp,
-        fontWeight = FontWeight.Bold,
-        lineHeight = 50.sp,
-        color = Color.Red,
-        modifier = modifier
-    )
+    Column(
+        modifier = modifier.fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ){
+        Text(
+            text = "Graduation Announcement",
+            textAlign = TextAlign.Center,
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            lineHeight = 50.sp,
+            color = Color.Red,
+            modifier = modifier
+        )
+        Image(
+            painter = painterResource(id = R.drawable.grad_cap),
+            contentDescription = "Graduation Cap",
+            alpha = 0.3f
+        )
+    }
 }
 
 @Preview(showBackground = true)
